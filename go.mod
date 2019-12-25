68962028dfa6a44dfcc35976c56aec279aa199ce
GLYASAI <chaoschunqfeijg@163.com>
2019-12-25 18:51:08 +0800

    add rcd Rainbond

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module goodrain/rainbond-operator
 go 1.13
 
 require (
+	github.com/coreos/etcd-operator v0.9.4 // indirect
 	github.com/operator-framework/operator-sdk v0.13.0
 	github.com/spf13/pflag v1.0.5
 	k8s.io/api v0.0.0
---
+++ pkg/apis/rainbond/v1alpha1/rainbond_types.go
@@ -0,0 +1,60 @@
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// RainbondSpec defines the desired state of Rainbond
+type RainbondSpec struct {
+	// Rainbond's version
+	Version string `json:"versoin,omitempty"`
+}
+
+// RainbondPhase is the Rainbond running phase
+type RainbondPhase string
+
+const (
+	// RainbondPhaseNone -
+	RainbondPhaseNone RainbondPhase = ""
+	// RainbondPhaseCreating -
+	RainbondPhaseCreating RainbondPhase = "Creating"
+	// RainbondPhaseRunning -
+	RainbondPhaseRunning RainbondPhase = "Running"
+	// RainbondPhaseFailed -
+	RainbondPhaseFailed RainbondPhase = "Failed"
+)
+
+// RainbondStatus defines the observed state of Rainbond
+type RainbondStatus struct {
+	// Phase is the rainbond running phase
+	Phase RainbondPhase `json:"phase"`
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// Rainbond is the Schema for the rainbonds API
+// +kubebuilder:subresource:status
+// +kubebuilder:resource:path=rainbonds,scope=Namespaced
+type Rainbond struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   RainbondSpec   `json:"spec,omitempty"`
+	Status RainbondStatus `json:"status,omitempty"`
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// RainbondList contains a list of Rainbond
+type RainbondList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []Rainbond `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&Rainbond{}, &RainbondList{})
+}
