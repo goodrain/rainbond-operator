a3066e8d2ca9fcf9d3039f457f3cde73c5af2269
GLYASAI <chaoschunqfeijg@163.com>
2019-12-25 18:12:13 +0800

    add crd PrivaetRegistry

---
+++ pkg/apis/privateregistry/group.go
@@ -0,0 +1,6 @@
+// Package privateregistry contains privateregistry API versions.
+//
+// This file ensures Go source parsers acknowledge the privateregistry package
+// and any child packages. It can be removed if any other Go source files are
+// added to this package.
+package privateregistry
---
+++ pkg/apis/privateregistry/v1alpha1/doc.go
@@ -0,0 +1,4 @@
+// Package v1alpha1 contains API Schema definitions for the privateregistry v1alpha1 API group
+// +k8s:deepcopy-gen=package,register
+// +groupName=privateregistry.rainbond.io
+package v1alpha1
