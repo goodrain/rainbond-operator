e3611a2d434aa38e752b5263405023b16b9350cc
GLYASAI <chaoschunqfeijg@163.com>
2019-12-26 12:36:26 +0800

    privateregistry controller

---
+++ pkg/controller/privateregistry/privateregistry_controller.go
@@ -0,0 +1,191 @@
+package privateregistry
+
+import (
+	"context"
+
+	rainbondv1alpha1 "github.com/GLYASAI/rainbond-operator/pkg/apis/rainbond/v1alpha1"
+
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/controller"
+	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
+	"sigs.k8s.io/controller-runtime/pkg/handler"
+	logf "sigs.k8s.io/controller-runtime/pkg/log"
+	"sigs.k8s.io/controller-runtime/pkg/manager"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
+	"sigs.k8s.io/controller-runtime/pkg/source"
+)
+
+var log = logf.Log.WithName("controller_privateregistry")
+
+// Add creates a new PrivateRegistry Controller and adds it to the Manager. The Manager will set fields on the Controller
+// and Start it when the Manager is Started.
+func Add(mgr manager.Manager) error {
+	return add(mgr, newReconciler(mgr))
+}
+
+// newReconciler returns a new reconcile.Reconciler
+func newReconciler(mgr manager.Manager) reconcile.Reconciler {
+	return &ReconcilePrivateRegistry{client: mgr.GetClient(), scheme: mgr.GetScheme()}
+}
+
+// add adds a new Controller to mgr with r as the reconcile.Reconciler
+func add(mgr manager.Manager, r reconcile.Reconciler) error {
+	// Create a new controller
+	c, err := controller.New("privateregistry-controller", mgr, controller.Options{Reconciler: r})
+	if err != nil {
+		return err
+	}
+
+	// Watch for changes to primary resource PrivateRegistry
+	err = c.Watch(&source.Kind{Type: &rainbondv1alpha1.PrivateRegistry{}}, &handler.EnqueueRequestForObject{})
+	if err != nil {
+		return err
+	}
+
+	// Watch for changes to secondary resource DaemonSets and requeue the owner PrivateRegistry
+	err = c.Watch(&source.Kind{Type: &appsv1.DaemonSet{}}, &handler.EnqueueRequestForOwner{
+		IsController: true,
+		OwnerType:    &rainbondv1alpha1.PrivateRegistry{},
+	})
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// blank assignment to verify that ReconcilePrivateRegistry implements reconcile.Reconciler
+var _ reconcile.Reconciler = &ReconcilePrivateRegistry{}
+
+// ReconcilePrivateRegistry reconciles a PrivateRegistry object
+type ReconcilePrivateRegistry struct {
+	// This client, initialized using mgr.Client() above, is a split client
+	// that reads objects from the cache and writes to the apiserver
+	client client.Client
+	scheme *runtime.Scheme
+}
+
+// Reconcile reads that state of the cluster for a PrivateRegistry object and makes changes based on the state read
+// and what is in the PrivateRegistry.Spec
+// Note:
+// The Controller will requeue the Request to be processed again if the returned error is non-nil or
+// Result.Requeue is true, otherwise upon completion it will remove the work from the queue.
+func (r *ReconcilePrivateRegistry) Reconcile(request reconcile.Request) (reconcile.Result, error) {
+	reqLogger := log.WithValues("Request.Namespace", request.Namespace, "Request.Name", request.Name)
+	reqLogger.Info("Reconciling PrivateRegistry")
+
+	// Fetch the PrivateRegistry instance
+	instance := &rainbondv1alpha1.PrivateRegistry{}
+	err := r.client.Get(context.TODO(), request.NamespacedName, instance)
+	if err != nil {
+		if errors.IsNotFound(err) {
+			// Request object not found, could have been deleted after reconcile request.
+			// Owned objects are automatically garbage collected. For additional cleanup logic use finalizers.
+			// Return and don't requeue
+			return reconcile.Result{}, nil
+		}
+		// Error reading the object - requeue the request.
+		return reconcile.Result{}, err
+	}
+
+	// Define a new daemonset
+	daemonSet := r.daemonSetForPrivateRegistry(instance)
+
+	// TODO: what for?
+	// Set PrivateRegistry instance as the owner and controller
+	if err := controllerutil.SetControllerReference(instance, daemonSet, r.scheme); err != nil {
+		return reconcile.Result{}, err
+	}
+
+	// Check if the daemonset already exists, if not create a new one
+	found := &appsv1.DaemonSet{}
+	err = r.client.Get(context.TODO(), types.NamespacedName{Name: instance.Name, Namespace: instance.Namespace}, found)
+	if err != nil && errors.IsNotFound(err) {
+		reqLogger.Info("Creating a new DaemonSet", "DaemonSet.Namespace", daemonSet.Namespace, "DaemonSet.Name", daemonSet.Name)
+		err = r.client.Create(context.TODO(), daemonSet)
+		if err != nil {
+			reqLogger.Error(err, "Failed to create new DaemonSet", "DaemonSet.Namespace", daemonSet.Namespace, "DaemonSet.Name", daemonSet.Name)
+			return reconcile.Result{}, err
+		}
+		// daemonset created successfully - return and requeue
+		return reconcile.Result{Requeue: true}, nil
+	} else if err != nil {
+		reqLogger.Error(err, "Failed to get DaemonSet")
+		return reconcile.Result{}, err
+	}
+
+	// DaemonSet already exists - don't requeue
+	reqLogger.Info("Skip reconcile: Pod already exists", "Pod.Namespace", found.Namespace, "Pod.Name", found.Name)
+	return reconcile.Result{}, nil
+}
+
+// daemonSetForPrivateRegistry returns a privateregistry Daemonset object
+func (r *ReconcilePrivateRegistry) daemonSetForPrivateRegistry(p *rainbondv1alpha1.PrivateRegistry) *appsv1.DaemonSet {
+	labels := labelsForPrivateRegistry(p.Name)
+	ds := &appsv1.DaemonSet{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "rbd-hub",
+			Namespace: p.Namespace, // TODO: can use custom namespace?
+			Labels:    labels,
+		},
+		Spec: appsv1.DaemonSetSpec{
+			Selector: &metav1.LabelSelector{
+				MatchLabels: labels,
+			},
+			Template: corev1.PodTemplateSpec{
+				ObjectMeta: metav1.ObjectMeta{
+					Name: "rbd-hub",
+					Labels: labels,
+				},
+				Spec: corev1.PodSpec{
+					Containers: []corev1.Container{
+						{
+							Name:            "rbd-hub",
+							Image:           "rainbond/rbd-registry:2.6.2",
+							ImagePullPolicy: corev1.PullIfNotPresent,
+							VolumeMounts: []corev1.VolumeMount{
+								{
+									Name:      "hubdata",
+									MountPath: "/var/lib/registry",
+								},
+							},
+						},
+					},
+					Volumes: []corev1.Volume{
+						{
+							Name: "hubdata",
+							VolumeSource: corev1.VolumeSource{
+								PersistentVolumeClaim: &corev1.PersistentVolumeClaimVolumeSource{
+									ClaimName: "hubdata",
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	return ds
+}
+
+// labelsForPrivateRegistry returns the labels for selecting the resources
+// belonging to the given PrivateRegistry CR name.
+func labelsForPrivateRegistry(name string) map[string]string {
+	return map[string]string{"name": "rbd-hub", "privateregistry_cr": name} // TODO: only one rainbond?
+}
+
+// getPodNames returns the pod names of the array of pods passed in
+func getPodNames(pods []corev1.Pod) []string {
+	var podNames []string
+	for _, pod := range pods {
+		podNames = append(podNames, pod.Name)
+	}
+	return podNames
+}
