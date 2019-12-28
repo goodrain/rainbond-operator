39d46f20e95a2dc951992d6ba4704340936f8637
GLYASAI <chaoschunqfeijg@163.com>
2019-12-28 18:01:10 +0800

    crd rbdcomponent and globalconfig

---
+++ pkg/apis/rainbond/v1alpha1/globalconfig_types.go
@@ -0,0 +1,63 @@
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// GlobalConfigSpec defines the desired state of GlobalConfig
+type GlobalConfigSpec struct {
+	// default goodrain.me
+	ImageRepositry string `json:"imageRepositry,omitempty"`
+	// the storage class that rainbond component will be used.
+	// rainbond-operator will create one if StorageClassName is empty
+	StorageClassName string `json:"storageClassName,omitempty"`
+	// the db connection information that rainbond component will be used.
+	// rainbond-operator will create one if DBInfo is empty
+	DBInfo DBInfo `json:"dbInfo,omitempty"`
+	// the etcd connection information that rainbond component will be used.
+	// rainbond-operator will create one if EtcdInfo is empty
+	EtcdInfo EtcdInfo `json:"etcdInfo,omitempty"`
+}
+
+// DBInfo defines the connection information of database.
+type DBInfo struct {
+	Host     string `json:"host"`
+	Port     int    `json:"port"`
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+// EtcdInfo defines the connection information of ETCD.
+type EtcdInfo struct {
+	Endpoints []string `json:"endpoints"`
+}
+
+// GlobalConfigStatus defines the observed state of GlobalConfig
+type GlobalConfigStatus struct {
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// GlobalConfig is the Schema for the globalconfigs API
+// +kubebuilder:subresource:status
+// +kubebuilder:resource:path=globalconfigs,scope=Namespaced
+type GlobalConfig struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   GlobalConfigSpec   `json:"spec,omitempty"`
+	Status GlobalConfigStatus `json:"status,omitempty"`
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// GlobalConfigList contains a list of GlobalConfig
+type GlobalConfigList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []GlobalConfig `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&GlobalConfig{}, &GlobalConfigList{})
+}
---
+++ pkg/apis/rainbond/v1alpha1/rbdcomponent_types.go
@@ -0,0 +1,68 @@
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// RbdComponentType is the type of rainbond component
+type RbdComponentType string
+
+const (
+	// RbdComponentTypeAPI rbd-api
+	RbdComponentTypeAPI RbdComponentType = "rbd-api"
+	// RbdComponentTypeWorker rbd-worker
+	RbdComponentTypeWorker RbdComponentType = "rbd-worker"
+)
+
+// LogLevel -
+type LogLevel string
+
+const (
+	// LogLevelDebug -
+	LogLevelDebug RbdComponentType = "debug"
+	// LogLevelInfo -
+	LogLevelInfo RbdComponentType = "info"
+	// LogLevelWarning -
+	LogLevelWarning RbdComponentType = "warning"
+	// LogLevelError -
+	LogLevelError RbdComponentType = "error"
+)
+
+// RbdComponentSpec defines the desired state of RbdComponent
+type RbdComponentSpec struct {
+	// type of rainbond component
+	Type RbdComponentType `json:"type"`
+	// version of rainbond component
+	Version string `json:"version"`
+	LogLevel   LogLevel              `json:"logLevel,omitempty"`
+}
+
+// RbdComponentStatus defines the observed state of RbdComponent
+type RbdComponentStatus struct {
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// RbdComponent is the Schema for the rbdcomponents API
+// +kubebuilder:subresource:status
+// +kubebuilder:resource:path=rbdcomponents,scope=Namespaced
+type RbdComponent struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   RbdComponentSpec   `json:"spec,omitempty"`
+	Status RbdComponentStatus `json:"status,omitempty"`
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// RbdComponentList contains a list of RbdComponent
+type RbdComponentList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []RbdComponent `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&RbdComponent{}, &RbdComponentList{})
+}
