a94694f6aa347c0b8f9e1775dc90faa63957b9d3
glyasai <chaoschunqfeijg@163.com>
2020-02-10 16:52:31 +0800

    fix metrics-server name

--- pkg/controller/rbdcomponent/add_handler.go
+++ pkg/controller/rbdcomponent/add_handler.go
@@ -22,5 +22,5 @@ func init() {
 	AddHandlerFunc(handler.DBName, handler.NewDB)
 	AddHandlerFunc(handler.WebCliName, handler.NewWebCli)
 	AddHandlerFunc(handler.GrctlName, handler.NewGrctl)
-	AddHandlerFunc(handler.GrctlName, handler.NewMetricsServer)
+	AddHandlerFunc(handler.MetricsServerName, handler.NewMetricsServer)
 }
