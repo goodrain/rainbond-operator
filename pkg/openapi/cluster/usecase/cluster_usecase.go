f0607d00b7971892b2836859abd209c4ec8b26a1
GLYASAI <chaoschunqfeijg@163.com>
2020-02-13 18:46:06 +0800

    default to without package

--- pkg/openapi/cluster/usecase/cluster_usecase.go
+++ pkg/openapi/cluster/usecase/cluster_usecase.go
@@ -226,7 +226,7 @@ func (c *ClusterUsecaseImpl) createCluster() (*rainbondv1alpha1.RainbondCluster,
 				URL: c.cfg.DownloadURL,
 				MD5: c.cfg.DownloadMD5,
 			},
-			InstallMode: rainbondv1alpha1.InstallationModeWithPackage,
+			InstallMode: rainbondv1alpha1.InstallationModeWithoutPackage,
 		},
 	}
 	return c.cfg.RainbondKubeClient.RainbondV1alpha1().RainbondClusters(c.cfg.Namespace).Create(cluster)
