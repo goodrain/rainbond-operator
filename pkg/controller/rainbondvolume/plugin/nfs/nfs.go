3c95656c407dd809b8c209462b12bdf0992e9511
GLYASAI <chaoschunqfeijg@163.com>
2020-03-12 15:44:28 +0800

    pv and pvc for nfs

--- pkg/controller/rainbondvolume/plugin/nfs/nfs.go
+++ pkg/controller/rainbondvolume/plugin/nfs/nfs.go
@@ -2,21 +2,22 @@ package nfs
 
 import (
 	"context"
-
-	"k8s.io/apimachinery/pkg/util/intstr"
+	"fmt"
 
 	rainbondv1alpha1 "github.com/goodrain/rainbond-operator/pkg/apis/rainbond/v1alpha1"
 	"github.com/goodrain/rainbond-operator/pkg/controller/rainbondvolume/plugin"
 	"github.com/goodrain/rainbond-operator/pkg/util/commonutil"
 	"github.com/goodrain/rainbond-operator/pkg/util/k8sutil"
 	"github.com/goodrain/rainbond-operator/pkg/util/rbdutil"
-	"k8s.io/apimachinery/pkg/api/errors"
-	"k8s.io/apimachinery/pkg/types"
-	"sigs.k8s.io/controller-runtime/pkg/client"
 
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/api/resource"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/apimachinery/pkg/util/intstr"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
 )
 
@@ -70,7 +71,9 @@ func (p *nfsPlugin) GetProvisioner() string {
 }
 
 func (p *nfsPlugin) GetClusterScopedResources() []interface{} {
-	return nil
+	return []interface{}{
+		p.pv(),
+	}
 }
 
 func (p *nfsPlugin) GetSubResources() []interface{} {
@@ -82,6 +85,7 @@ func (p *nfsPlugin) GetSubResources() []interface{} {
 
 func (p *nfsPlugin) statefulset() interface{} {
 	labels := p.labels
+	pvc := p.pvc()
 	sts := &appsv1.StatefulSet{
 		ObjectMeta: metav1.ObjectMeta{
 			Name:      p.name,
@@ -198,25 +202,15 @@ func (p *nfsPlugin) statefulset() interface{} {
 							},
 							VolumeMounts: []corev1.VolumeMount{
 								{
-									Name:      "export-volume",
+									Name:      "data",
 									MountPath: "/export",
-								},
-							},
-						},
-					},
-					Volumes: []corev1.Volume{
-						{
-							Name: "export-volume",
-							VolumeSource: corev1.VolumeSource{
-								HostPath: &corev1.HostPathVolumeSource{
-									Path: "/opt/rainbond/data/nfs",
-									Type: k8sutil.HostPath(corev1.HostPathDirectoryOrCreate),
 								},
 							},
 						},
 					},
 				},
 			},
+			VolumeClaimTemplates: []corev1.PersistentVolumeClaim{*pvc},
 		},
 	}
 
@@ -310,4 +304,93 @@ func (p *nfsPlugin) service() *corev1.Service {
 	}
 
 	return svc
+}
+
+func (p *nfsPlugin) pv() *corev1.PersistentVolume {
+	nodeList := &corev1.NodeList{}
+	if err := p.cli.List(p.ctx, nodeList); err != nil {
+		log.V(3).Info(fmt.Sprintf("list nodes: %v", err))
+	}
+	var largeStorageNode *corev1.Node
+	for idx := range nodeList.Items {
+		node := nodeList.Items[idx]
+		if largeStorageNode == nil {
+			largeStorageNode = &node
+			continue
+		}
+		if node.Status.Capacity.StorageEphemeral().MilliValue() > largeStorageNode.Status.Capacity.StorageEphemeral().MilliValue() {
+			largeStorageNode = &node
+		}
+	}
+
+	pv := &corev1.PersistentVolume{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:   p.name,
+			Labels: p.labels,
+		},
+	}
+
+	var affnity *corev1.VolumeNodeAffinity
+	if largeStorageNode != nil {
+		affnity = &corev1.VolumeNodeAffinity{
+			Required: &corev1.NodeSelector{
+				NodeSelectorTerms: []corev1.NodeSelectorTerm{
+					{
+						MatchExpressions: []corev1.NodeSelectorRequirement{
+							{
+								Key:      "kubernetes.io/hostname",
+								Operator: corev1.NodeSelectorOpIn,
+								Values:   []string{largeStorageNode.Name},
+							},
+						},
+					},
+				},
+			},
+		}
+	}
+
+	size := resource.NewQuantity(1*1024*1024*1024, resource.BinarySI)
+	spec := corev1.PersistentVolumeSpec{
+		AccessModes: []corev1.PersistentVolumeAccessMode{
+			corev1.ReadWriteMany,
+		},
+		Capacity: corev1.ResourceList{
+			corev1.ResourceStorage: *size,
+		},
+	}
+	if affnity != nil {
+		spec.NodeAffinity = affnity
+	}
+
+	hostPath := &corev1.HostPathVolumeSource{
+		Path: "/opt/rainbond/data/nfs",
+		Type: k8sutil.HostPath(corev1.HostPathDirectoryOrCreate),
+	}
+	spec.HostPath = hostPath
+
+	pv.Spec = spec
+
+	return pv
+}
+
+func (p *nfsPlugin) pvc() *corev1.PersistentVolumeClaim {
+	size := resource.NewQuantity(1*1024*1024*1024, resource.BinarySI)
+	pvc := &corev1.PersistentVolumeClaim{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:   "data",
+			Labels: p.labels,
+		},
+		Spec: corev1.PersistentVolumeClaimSpec{
+			AccessModes: []corev1.PersistentVolumeAccessMode{
+				corev1.ReadWriteMany,
+			},
+			Resources: corev1.ResourceRequirements{
+				Requests: map[corev1.ResourceName]resource.Quantity{
+					corev1.ResourceStorage: *size,
+				},
+			},
+			VolumeName: p.name,
+		},
+	}
+	return pvc
 }
