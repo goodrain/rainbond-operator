f7f4bccc99a8a465397037e4ebee044d0706efda
GLYASAI <chaoschunqfeijg@163.com>
2020-04-13 14:32:59 +0800

    make unit test ok

--- pkg/controller/rainbondcluster/rainbondcluster_test.go
+++ pkg/controller/rainbondcluster/rainbondcluster_test.go
@@ -3,32 +3,11 @@ package rainbondcluster
 import (
 	"testing"
 
-	"github.com/bmizerany/assert"
 	rainbondv1alpha1 "github.com/goodrain/rainbond-operator/pkg/apis/rainbond/v1alpha1"
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client/fake"
 )
-
-func TestGenerateDockerConfig(t *testing.T) {
-	want := "eyJhdXRocyI6eyJnb29kcmFpbi5tZSI6eyJhdXRoIjoiWVdSdGFXNDZaMjl2WkhKaGFXND0iLCJwYXNzd29yZCI6Imdvb2RyYWluIiwidXNlcm5hbWUiOiJhZG1pbiJ9fX0="
-
-	cluster := &rainbondv1alpha1.RainbondCluster{
-		Spec: rainbondv1alpha1.RainbondClusterSpec{
-			ImageHub: &rainbondv1alpha1.ImageHub{
-				Domain:   "goodrain.me",
-				Username: "admin",
-				Password: "goodrain",
-			},
-		},
-	}
-	mgr := rainbondClusteMgr{
-		cluster: cluster,
-	}
-	got := mgr.generateDockerConfig()
-
-	assert.Equal(t, want, string(got))
-}
 
 func TestCreateImagePullSecret(t *testing.T) {
 	scheme := runtime.NewScheme()
@@ -48,6 +27,7 @@ func TestCreateImagePullSecret(t *testing.T) {
 	mgr := rainbondClusteMgr{
 		cluster: cluster,
 		client:  cli,
+		scheme:  scheme,
 	}
 
 	err := mgr.createImagePullSecret()
