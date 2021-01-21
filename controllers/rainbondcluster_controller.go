a208c8fb90e983aa6c890a628742de9eb6e46dfd
barnettZQG <zengqg@goodrain.com>
2021-01-21 16:55:35 +0800

    change cluster update status

--- controllers/rainbondcluster_controller.go
+++ controllers/rainbondcluster_controller.go
@@ -66,10 +66,14 @@ func (r *RainbondClusterReconciler) Reconcile(ctx context.Context, request ctrl.
 		reqLogger.Error(err, "failed to generate rainbondcluster status")
 		return reconcile.Result{RequeueAfter: time.Second * 2}, err
 	}
-	rainbondcluster.Status = *status
 
 	if err := retry.RetryOnConflict(retry.DefaultRetry, func() error {
-		return r.Status().Update(ctx, rainbondcluster)
+		rc := &rainbondv1alpha1.RainbondCluster{}
+		if err := r.Get(ctx, request.NamespacedName, rc); err != nil {
+			return err
+		}
+		rc.Status = *status
+		return r.Status().Update(ctx, rc)
 	}); err != nil {
 		reqLogger.Error(err, "update rainbondcluster status")
 		return reconcile.Result{RequeueAfter: time.Second * 2}, err
