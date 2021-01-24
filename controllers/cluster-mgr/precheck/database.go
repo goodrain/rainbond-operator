ca6640a77593885c3bf91a918d75525b3ed6913a
barnettZQG <zengqg@goodrain.com>
2021-01-24 20:51:23 +0800

    import mysql driver

--- controllers/cluster-mgr/precheck/database.go
+++ controllers/cluster-mgr/precheck/database.go
@@ -4,7 +4,7 @@ import (
 	"database/sql"
 	"fmt"
 	"time"
-
+	_ "github.com/go-sql-driver/mysql"
 	rainbondv1alpha1 "github.com/goodrain/rainbond-operator/api/v1alpha1"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
