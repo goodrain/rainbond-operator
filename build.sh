9e5452a23b436fa78508d2d5b2e7f21c8800c038
dingpeng24001 <dingpeng24001@talkweb.com.cn>
2022-03-11 10:17:13 +0800

    Merge branch 'master' into dev

---
+++ build.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+# References:
+# 1. https://docs.docker.com/buildx/working-with-buildx/
+# 2. https://docs.docker.com/engine/reference/commandline/buildx/
+
+# Requirements:
+# 1. docker run --privileged --rm tonistiigi/binfmt --install all
+# 2. docker login dockerhub
+# 3. docker login myhuaweicloud-swr
+
+export NAMESPACE=wutong-operator
+export VERSION=v1.0.0-stable
+docker buildx create --use --name adaptorbuilder
+docker buildx build --platform linux/amd64,linux/arm64 --push -t wutongpaas/${NAMESPACE}:${VERSION} -f Dockerfile . 
+docker buildx build --platform linux/amd64,linux/arm64 --push -t swr.cn-southwest-2.myhuaweicloud.com/wutong/${NAMESPACE}:${VERSION} -f Dockerfile . 
+docker buildx rm adaptorbuilder
