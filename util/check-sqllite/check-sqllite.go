6af128b8806c5fdf37349547e89488cfcafc54dd
Quyc <67988480+quyuancheng@users.noreply.github.com>
2022-10-09 20:51:42 +0800

    fix: is sqlite log remove (#163)
    * fix: delete hostsfile exist log
    
    * fix: use nodesForGateway internalIP config
    
    * fix: is sqlite log remove
    
    Co-authored-by: 曲源成 <quyc@goodrain.com>

--- util/check-sqllite/check-sqllite.go
+++ util/check-sqllite/check-sqllite.go
@@ -1,15 +1,12 @@
 package check_sqllite
 
 import (
-	"github.com/sirupsen/logrus"
 	"os"
 )
 
 func IsSQLLite() bool {
 	if os.Getenv("IS_SQLLITE") != "" {
-		logrus.Info("IS SQLLITE")
 		return true
 	}
-	logrus.Info("IS MYSQL")
 	return false
 }
