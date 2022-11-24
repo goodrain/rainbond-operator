9238bb9054c2fd2c7f461330bc1e20c49e415799
zhangsetsail <zqh15131121078@126.com>
2022-11-24 21:12:07 +0800

    fix: modify the default container runtime to Docker

--- util/containerutil/containerutil.go
+++ util/containerutil/containerutil.go
@@ -12,16 +12,16 @@ const (
 // GetContainerRuntime get container runtime
 func GetContainerRuntime() string {
 	if os.Getenv("CONTAINER_RUNTIME") != "" {
-		if os.Getenv("CONTAINER_RUNTIME") == ContainerRuntimeDocker {
-			return ContainerRuntimeDocker
+		if os.Getenv("CONTAINER_RUNTIME") == ContainerRuntimeContainerd {
+			return ContainerRuntimeContainerd
 		}
-		return ContainerRuntimeContainerd
-	}
-	// The containerd runtime is used by default
-	_, err := os.Stat("/run/containerd/containerd.sock")
-	if err != nil {
-		// no such file or dir, Indicates that the user does not have containerd installed and uses docker as the runtime
 		return ContainerRuntimeDocker
 	}
-	return ContainerRuntimeContainerd
+	// The containerd runtime is used by default
+	_, err := os.Stat("/var/run/docker.sock")
+	if err != nil {
+		// no such file or dir, Indicates that the user does not have containerd installed and uses docker as the runtime
+		return ContainerRuntimeContainerd
+	}
+	return ContainerRuntimeDocker
 }
