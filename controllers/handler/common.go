af28a21c3ab6ceff182e04148b4c2005abf4f2e3
曲源成 <quyc@goodrain.com>
2023-03-21 23:25:57 +0800

    fix: monitor storage request

--- controllers/handler/common.go
+++ controllers/handler/common.go
@@ -297,7 +297,7 @@ func createPersistentVolumeClaimRWO(ns, claimName string, pvcParameters *pvcPara
 }
 
 func createPersistentVolumeClaim(ns, claimName string, accessModes []corev1.PersistentVolumeAccessMode, pvcParameters *pvcParameters, labels map[string]string, storageRequest int64) *corev1.PersistentVolumeClaim {
-	if pvcParameters.storageRequest != nil {
+	if pvcParameters.storageRequest != nil && storageRequest == 0 {
 		storageRequest = int64(*pvcParameters.storageRequest)
 	}
 	size := resource.NewQuantity(storageRequest*1024*1024*1024, resource.BinarySI)
