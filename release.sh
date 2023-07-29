95d833d8ad327ceee5e3ed45f948435228b33411
Qi Zhang <smallqi1@163.com>
2023-07-29 21:35:08 +0800

    feat: change packing script
    Signed-off-by: Qi Zhang <smallqi1@163.com>

--- release.sh
+++ release.sh
@@ -5,12 +5,18 @@ IMAGE_NAMESPACE=${IMAGE_NAMESPACE:-'rainbond'}
 DOMESTIC_BASE_NAME=${DOMESTIC_BASE_NAME:-'registry.cn-hangzhou.aliyuncs.com'}
 DOMESTIC_NAMESPACE=${DOMESTIC_NAMESPACE:-'goodrain'}
 
-imageName=${IMAGE_DOMAIN}/${IMAGE_NAMESPACE}/rainbond-operator:${VERSION}
-docker build -t "${imageName}" -f Dockerfile .
-echo "$DOCKER_PASSWORD" | docker login ${IMAGE_DOMAIN} -u "$DOCKER_USERNAME" --password-stdin
-docker push ${imageName}
 
-domestcName=${DOMESTIC_BASE_NAME}/${DOMESTIC_NAMESPACE}/rainbond-operator:${VERSION}
-docker tag "${imageName}" "${domestcName}"
-echo "${DOMESTIC_DOCKER_PASSWORD}"|docker login -u "${DOMESTIC_DOCKER_USERNAME}" "${DOMESTIC_BASE_NAME}" --password-stdin
-docker push "${domestcName}"
+imageName=${IMAGE_DOMAIN}/${IMAGE_NAMESPACE}/rainbond-operator:${VERSION}
+docker build --build-arg ARCH="${ARCH}" -t "${imageName}" -f Dockerfile .
+
+if [ "$DOCKER_USERNAME" ]; then
+  echo "$DOCKER_PASSWORD" | docker login ${IMAGE_DOMAIN} -u "$DOCKER_USERNAME" --password-stdin
+  docker push "${imageName}"
+fi
+
+if [ "$DOMESTIC_DOCKER_USERNAME" ]; then
+  domestcName=${DOMESTIC_BASE_NAME}/${DOMESTIC_NAMESPACE}/rainbond-operator:${VERSION}
+  docker tag "${imageName}" "${domestcName}"
+  echo "${DOMESTIC_DOCKER_PASSWORD}"|docker login -u "${DOMESTIC_DOCKER_USERNAME}" "${DOMESTIC_BASE_NAME}" --password-stdin
+  docker push "${domestcName}"
+fi
