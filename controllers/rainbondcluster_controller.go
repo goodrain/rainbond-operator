77dcd7cc53b79eaa0aa310569c1d63837291730c
逆流而上 <62740231+lengyue-ck@users.noreply.github.com>
2023-08-24 10:37:38 +0800

    fix: change dns server (#207)

--- controllers/rainbondcluster_controller.go
+++ controllers/rainbondcluster_controller.go
@@ -3,31 +3,28 @@ package controllers
 import (
 	"context"
 	"fmt"
-	"net"
-	"os"
-	"strings"
-	"time"
-
-	"github.com/goodrain/rainbond-operator/util/commonutil"
-	"github.com/goodrain/rainbond-operator/util/rbdutil"
-	"github.com/goodrain/rainbond-operator/util/retryutil"
-	"github.com/goodrain/rainbond-operator/util/suffixdomain"
-	"github.com/sirupsen/logrus"
-	"k8s.io/apimachinery/pkg/types"
-
 	"github.com/go-logr/logr"
 	rainbondv1alpha1 "github.com/goodrain/rainbond-operator/api/v1alpha1"
 	clustermgr "github.com/goodrain/rainbond-operator/controllers/cluster-mgr"
+	"github.com/goodrain/rainbond-operator/util/commonutil"
 	"github.com/goodrain/rainbond-operator/util/constants"
+	"github.com/goodrain/rainbond-operator/util/rbdutil"
+	"github.com/goodrain/rainbond-operator/util/suffixdomain"
 	"github.com/goodrain/rainbond-operator/util/uuidutil"
 	"github.com/juju/errors"
+	"github.com/sirupsen/logrus"
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/client-go/tools/record"
 	"k8s.io/client-go/util/retry"
+	"net"
+	"os"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
+	"strings"
+	"time"
 )
 
 // RainbondClusterReconciler reconciles a RainbondCluster object
@@ -197,21 +194,7 @@ func (r *RainbondClusterReconciler) Reconcile(ctx context.Context, request ctrl.
 			ip = rainbondcluster.Spec.GatewayIngressIPs[0]
 		}
 		if ip != "" {
-			err := retryutil.Retry(1*time.Second, 3, func() (bool, error) {
-				domain, err := r.genSuffixHTTPHost(ip, rainbondcluster)
-				if err != nil {
-					return false, err
-				}
-				rainbondcluster.Spec.SuffixHTTPHost = domain
-				if !strings.HasSuffix(domain, constants.DefHTTPDomainSuffix) {
-					rainbondcluster.Spec.SuffixHTTPHost = constants.DefHTTPDomainSuffix
-				}
-				return true, nil
-			})
-			if err != nil {
-				logrus.Warningf("generate suffix http host: %v", err)
-				rainbondcluster.Spec.SuffixHTTPHost = constants.DefHTTPDomainSuffix
-			}
+			rainbondcluster.Spec.SuffixHTTPHost = ip + rbdutil.GetenvDefault("DNS_SERVER", ".nip.io")
 			rc := &rainbondv1alpha1.RainbondCluster{}
 			if err := r.Get(ctx, request.NamespacedName, rc); err != nil {
 				return reconcile.Result{RequeueAfter: time.Second * 1}, err
@@ -289,7 +272,7 @@ func (r *RainbondClusterReconciler) getOrCreateUUIDAndAuth(rainbondcluster *rain
 	return cm.Data["uuid"], cm.Data["auth"], nil
 }
 
-//GetRainbondGatewayNodeAndChaosNodes get gateway nodes
+// GetRainbondGatewayNodeAndChaosNodes get gateway nodes
 func (r *RainbondClusterReconciler) GetRainbondGatewayNodeAndChaosNodes() (gatewayNodes, chaosNodes []*rainbondv1alpha1.K8sNode) {
 	nodeList := &corev1.NodeList{}
 	reqLogger := r.Log.WithValues("rainbondcluster", types.NamespacedName{Name: "rbd-system"})
