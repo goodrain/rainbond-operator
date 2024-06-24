58a9d036a3f2a51a25f4944d1bebc4c49197f5a9
paterl <117519642+paterleng@users.noreply.github.com>
2024-06-24 10:17:13 +0800

    perf: component support use private repository image (#252)

--- controllers/handler/common.go
+++ controllers/handler/common.go
@@ -35,6 +35,8 @@ const (
 	RegionDatabaseName = "region"
 	// ConsoleDatabaseName -
 	ConsoleDatabaseName = "console"
+	// RdbCustomImageRepository
+	RdbCustomImageRepository = "custom-image-repository"
 )
 
 // pvcParameters holds parameters to create pvc.
@@ -429,6 +431,9 @@ func imagePullSecrets(cpt *rainbondv1alpha1.RbdComponent, cluster *rainbondv1alp
 		return nil
 	}
 	return []corev1.LocalObjectReference{
+		{
+			RdbCustomImageRepository,
+		},
 		*cluster.Status.ImagePullSecret,
 	}
 }
