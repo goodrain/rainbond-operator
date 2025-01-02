4f3aca5bf14dd526dda06012b9b144a8e757a89f
Qi Zhang <smallqi1@163.com>
2025-01-03 00:04:35 +0800

    fix: missing package import
    Signed-off-by: Qi Zhang <smallqi1@163.com>

--- controllers/handler/app-ui.go
+++ controllers/handler/app-ui.go
@@ -15,6 +15,7 @@ import (
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/api/resource"
 	"k8s.io/apimachinery/pkg/util/intstr"
 	"os"
 	"runtime"
