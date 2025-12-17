b1f2553d0d59903efa208bdf0377de4f0c580766
张启航 <12344192+zhangsetsail@user.noreply.gitee.com>
2025-12-17 14:53:55 +0800

    fix: change api LivenessProbe
    Signed-off-by: 张启航 <12344192+zhangsetsail@user.noreply.gitee.com>

--- controllers/handler/api.go
+++ controllers/handler/api.go
@@ -173,7 +173,6 @@ func (a *api) deployment() client.Object {
 	volumes = mergeVolumes(volumes, a.component.Spec.Volumes)
 
 	// prepare probe
-	readinessProbe := probeutil.MakeReadinessProbeHTTP("", "/v2/health", 8888)
 	ds := &appsv1.Deployment{
 		ObjectMeta: metav1.ObjectMeta{
 			Name:      APIName,
@@ -202,8 +201,20 @@ func (a *api) deployment() client.Object {
 							Env:             envs,
 							Args:            args,
 							VolumeMounts:    volumeMounts,
-							ReadinessProbe:  readinessProbe,
-							Resources:       a.component.Spec.Resources,
+							ReadinessProbe: &corev1.Probe{
+								Handler: corev1.Handler{
+									HTTPGet: &corev1.HTTPGetAction{
+										Path: "/v2/health",
+										Port: intstr.FromInt(8888),
+									},
+								},
+								InitialDelaySeconds: 5,
+								TimeoutSeconds:      3,
+								PeriodSeconds:       3,
+								SuccessThreshold:    1,
+								FailureThreshold:    6,
+							},
+							Resources: a.component.Spec.Resources,
 							LivenessProbe: &corev1.Probe{
 								Handler: corev1.Handler{
 									HTTPGet: &corev1.HTTPGetAction{
@@ -211,11 +222,11 @@ func (a *api) deployment() client.Object {
 										Port: intstr.FromInt(8889),
 									},
 								},
-								InitialDelaySeconds: 30,
-								TimeoutSeconds:      2,
-								PeriodSeconds:       10,
+								InitialDelaySeconds: 60,
+								TimeoutSeconds:      10,
+								PeriodSeconds:       30,
 								SuccessThreshold:    1,
-								FailureThreshold:    3,
+								FailureThreshold:    5,
 							},
 						},
 					},
